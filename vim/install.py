1c293a2fa3123d63b7b8072b117bd64d093c4c87
Kevin Steffler <Kevin@Annas-MacBook-Pro.local>
2013-03-03 13:17:44 -0700

    big vim push

--- vim/install.py
+++ vim/install.py
@@ -1,38 +1,38 @@
+#!/usr/bin/python2.7
+
+
 import subprocess
 import os
+
 
 CYAN = os.environ["ECHO_CYAN"]
 DEFAULT = os.environ["ECHO_DEFAULT"]
 PURPLE = os.environ["ECHO_PURPLE"]
 
-def run():
-    source = "{}{sep}vim".format(os.environ["KFS_SETTINGS"], sep=os.sep)
-    home = os.environ["HOME"]
 
-    oldrc = "{}{sep}.vimrc".format(home, sep=os.sep)
-    olddotvim ="{}{sep}.vim".format(home, sep=os.sep)
+HOME = os.environ["HOME"]
+HOME_VIMRC = os.path.join(HOME, ".vimrc")
+HOME_VIM_FILES = os.path.join(HOME, ".vim")
+KFS_SETTINGS = os.environ["KFS_SETTINGS"]
+KFS_VIM = os.path.join(KFS_SETTINGS, "vim")
+KFS_VIMRC = os.path.join(KFS_VIM, "vimrc")
+KFS_VIM_FILES = os.path.join(KFS_VIM, "vim_files")
 
-    if os.path.exists(oldrc):
-        with open(oldrc) as f:
-            line = f.readline()
-            if line[:-1] == '" --- KFS VIM SETTINGS ---':
-                return
-        make_backup(oldrc)
-        make_backup(olddotvim)
 
-    command = "cp {source}{sep}vimrc {path}{sep}.vimrc".format(source=source, sep=os.sep, path=home)
-    subprocess.check_call(command, shell=True)
+def main():
+    vimrc()
+    vim_files()
 
-    command = "cp -r {source}{sep}.vim {path}".format(source=source, sep=os.sep, path=home)
-    subprocess.check_call(command, shell=True)
 
-def make_backup(path):
-    backup = "{}.backup".format(path)
-    command = "echo 'Making backup of file(s):: {cyan}{f} {default}({purple}{backup}{default})'"
-    command = command.format(cyan=CYAN, f=path, default=DEFAULT, purple=PURPLE, backup=backup)
-    subprocess.check_call(command, shell=True)
-    command = "mv {f} {backup}".format(f=path, backup=backup)
-    subprocess.check_call(command, shell=True)
+def vimrc():
+    with open(HOME_VIMRC, "w") as f:
+        f.write("source {}".format(KFS_VIMRC))
+
+
+def vim_files():
+    subprocess.check_call("rm -f {}".format(HOME_VIM_FILES), shell=True)
+    subprocess.check_call("ln -s {} {}".format(KFS_VIM_FILES, HOME_VIM_FILES), shell=True)
+
 
 if __name__ == "__main__":
-    run()
+    main()
